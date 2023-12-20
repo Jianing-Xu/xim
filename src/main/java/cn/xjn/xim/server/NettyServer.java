01624af731868b5722c9b3bd8083322f3486ed59
xjn <240390391@qq.com>
2023-12-20 20:35:24 +0800

    feat:Netty server startup

---
+++ src/main/java/cn/xjn/xim/server/NettyServer.java
@@ -0,0 +1,46 @@
+package cn.xjn.xim.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author xjn
+ * @date 2023-12-20
+ */
+@Slf4j
+public class NettyServer {
+
+    private static final int PORT = 8000;
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap
+                .group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+
+                    }
+                });
+
+        bind(serverBootstrap, PORT);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                log.info("The Netty server started up successfully.");
+            } else {
+                log.error("The Netty server bind port[{}] failed", port);
+            }
+        });
+    }
+}
